997b3eb15061fa9be99d0a604ccf6ede0bdef444
Kavli <lzylzu@qq.com>
2017-11-27 11:19:08 +0800

    lesson01

---
+++ springcloudserver/src/main/java/com/maxfun/start/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.maxfun.start;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by Administrator on 2017/11/27.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
